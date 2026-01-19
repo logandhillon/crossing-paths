3f3b1ee35737a6029542de54e13e5e9c6e8fc450
Logan Dhillon <dev@logandhillon.com>
2026-01-19 02:23:26 -0500

    feat: level names & difficulties

--- src/main/java/com/logandhillon/fptgame/resource/Levels.java
+++ src/main/java/com/logandhillon/fptgame/resource/Levels.java
@@ -1,8 +1,8 @@
 package com.logandhillon.fptgame.resource;
 
+import com.logandhillon.fptgame.entity.game.LevelButtonEntity;
 import com.logandhillon.fptgame.entity.game.MovingPlatformEntity;
 import com.logandhillon.fptgame.entity.game.PlatformEntity;
-import com.logandhillon.fptgame.entity.game.LevelButtonEntity;
 import com.logandhillon.fptgame.entity.game.PortalEntity;
 import com.logandhillon.fptgame.networking.proto.LevelProto;
 
@@ -12,16 +12,24 @@ import com.logandhillon.fptgame.networking.proto.LevelProto;
 public class Levels {
     public static final LevelProto.LevelData DEBUG_LEVEL_2 = LevelProto.LevelData
             .newBuilder()
+            .setLevelName("DEBUG LEVEL 2")
             .setBackground(Textures.UNDERGROUND_BG.serialize())
+
             .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 0, 720 - 40, 1280, 40).serialize())
             .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 400, 560, 320, 40).serialize())
             .addObjects(new PlatformEntity(Textures.UNDERGROUND_PIPE, 300, 200, 40, 320).serialize())
             .addObjects(new PortalEntity(900, 500, true).serialize())
+
             .build();
 
     public static final LevelProto.LevelData DEBUG_LEVEL = LevelProto.LevelData
             .newBuilder()
+            .setLevelName("DEBUG LEVEL 1")
+            .setNextLevel(DEBUG_LEVEL_2)
             .setBackground(Textures.UNDERGROUND_BG.serialize())
+
+            .setPlayer1SpawnX(100).setPlayer1SpawnY(600)
+
             .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 0, 720 - 40, 1280, 40).serialize())
             .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 200, 600, 320, 40).serialize())
             .addObjects(
@@ -32,7 +40,6 @@ public class Levels {
                                                  false, LevelProto.Color.NONE).serialize())
             .addObjects(new PortalEntity(800, 500, true).serialize())
             .addObjects(new LevelButtonEntity(50, 660).serialize())
-            .setNextLevel(DEBUG_LEVEL_2)
-            .setPlayer1SpawnX(100).setPlayer1SpawnY(600)
+
             .build();
 }
