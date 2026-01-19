dd9474633d8576137476edc80184527b2a3dfd4d
Logan Dhillon <dev@logandhillon.com>
2026-01-19 02:37:43 -0500

    feat: create level 1

--- src/main/java/com/logandhillon/fptgame/resource/Levels.java
+++ src/main/java/com/logandhillon/fptgame/resource/Levels.java
@@ -42,4 +42,34 @@ public class Levels {
             .addObjects(new LevelButtonEntity(50, 660).serialize())
 
             .build();
+
+    public static final LevelProto.LevelData LEVEL_1 = LevelProto.LevelData
+            .newBuilder()
+            .setLevelName("Staircase")
+            .setLevelDifficulty("Easy")
+//            .setNextLevel(DEBUG_LEVEL_2)
+            .setBackground(Textures.UNDERGROUND_BG.serialize())
+
+            .setPlayer1SpawnX(80).setPlayer1SpawnY(608)
+            .setPlayer2SpawnX(1158).setPlayer2SpawnY(608)
+
+            // floor
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 0, 720 - 40, 1280, 40).serialize())
+
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 270, 488, 80, 40).serialize())
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 0, 392, 80, 40).serialize())
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 270, 296, 80, 40).serialize())
+
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 540, 200, 200, 40).serialize())
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 540, 584, 200, 40).serialize())
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_PIPE, 620, 240, 40, 440).serialize())
+
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 930, 488, 80, 40).serialize())
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 1200, 392, 80, 40).serialize())
+            .addObjects(new PlatformEntity(Textures.UNDERGROUND_BRICKS, 930, 296, 80, 40).serialize())
+
+            .addObjects(new PortalEntity(550, 100, true).serialize())
+            .addObjects(new PortalEntity(680, 100, false).serialize())
+
+            .build();
 }
